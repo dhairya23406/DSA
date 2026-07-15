86ae3c6ca7314947e6cde0df5b67479b7b4f4f15
dhairya23406 <dhairyajha23406@gmail.com>
2026-07-15 12:27:20 +0530

    Time: 1 ms (18.27%) | Memory: 9.3 MB (11.79%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        unordered_map<char,char> closetoopen = {
+            {']','['},
+            {')','('},
+            {'}','{'}
+        };
+
+
+        for(char c:s){
+            if(closetoopen.count(c)){
+                if(!st.empty() && st.top() == closetoopen[c]){
+                    st.pop();
+                }else return false;
+            }
+            else st.push(c);
+            
+        }
+        
+        return st.empty();
+    }
+};
